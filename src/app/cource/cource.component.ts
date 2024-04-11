46d1fe9ef21b20ba099344c4459225994e75b284
niyatee_014 <niyatee.sahoo@iserveu.co.in>
2024-04-11 11:16:47 +0530

    new course added

--- src/app/cource/cource.component.ts
+++ src/app/cource/cource.component.ts
@@ -15,43 +15,45 @@ export class CourceComponent {
   courseArray=[
     {
       id:"1",
-      name:'CSIR-NET',
-      price:7500,
-      course:'NET:- National Eligibility Test',
+      name:'GATE NET GSI ',
+      price:9999,
+      course:'NET (Dec-2024), GATE, GSI Prelims 2025',
       description:'Prepare for the CSIR-NET exam with our comprehensive courses designed specifically for aspiring candidates like you. Our expert-developed curriculum covers all the key subjects and provides in-depth study material, practice questions....................',
       img:'../../assets/csir_img.webp'
     },
     {
       id:"2",
-      name:'GATE',
-      price:6500,
-      course:'GATE: Graduate Aptitude Test in Engineering',
+      name:'GATE NET',
+      price:8499,
+      course:'NET (Dec-2024), GATE 2025',
       description:'Join our community of motivated learners, access our top-quality resources, and pave your path to success in the GATE exam, opening doors to exciting postgraduate opportunities and promising career prospects. Start your GATE journey with us ..................',
       img:'../../assets/gate_img.webp'
     },
     {
       id:"3",
-      name:'UPSC-GSI',
-      price:9000,
-      course:'UPSC/GSI: Union Public Service Commission/Geological Survey of India',
-      description:'The GSI is a renowned government organization responsible for geological mapping, mineral exploration, and research across the country. By successfully clearing the UPSC-GSI examination, candidates can secure prestigious roles such as ............',
-      img:'../../assets/upsc.jpg'
+      name:'IIT-JAM',
+      price:7500,
+      course:'IIT-JAM/BHU/DU 2024-25',
+      description:'The IIT-JAM (Joint Admission Test for M.Sc.) is a national-level entrance examination conducted by the Indian Institutes of Technology (IITs) for admission to various postgraduate programs in Science. It serves as a gateway for aspiring students to pursue Masters  ..........',
+      img:'../../assets/iitjam.png'
     },
     {
       id:"4",
-      name:'IIT-JAM',
-      price:5500,
-      course:'IIT-JAM: Joint Admission Test',
-      description:'The IIT-JAM (Joint Admission Test for M.Sc.) is a national-level entrance examination conducted by the Indian Institutes of Technology (IITs) for admission to various postgraduate programs in Science. It serves as a gateway for aspiring students to pursue Masters  ..........',
-      img:'../../assets/iitjam.png'
-    }
+      name:'TEST SERIES',
+      price:9000,
+      course:'Get The Competitive Edege with our Geology Prep Course',
+      img:'../../assets/upsc.jpg'
+    },
   ]
   knowmore(e:any){
-    const dialogRef= this.dialog.open(KnowmorwContentComponent);
+    const dialogRef= this.dialog.open(KnowmorwContentComponent,{
+      height:'450px',
+      width:'800px'
+    });
     console.log(dialogRef);
     this.service.sendData({itemId:e});
-    
-    
+
+
   }
 
 }
